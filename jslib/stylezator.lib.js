19c770d58b165f1507e69b2c1300e2d8679939f4
Jahongir Sobirov <69193276+Jahongir2007@users.noreply.github.com>
2021-03-28 18:23:47 +0500

    Create stylezator.lib.js

---
+++ jslib/stylezator.lib.js
@@ -0,0 +1,32 @@
+function dropdown(selector){
+    document.querySelector(selector).classList.toggle("show");
+    window.onclick = function(event) {
+        if (!event.target.matches('.dropbtn')) {
+          var dropdowns = document.getElementsByClassName("dropdown-content");
+          var i;
+          for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+              openDropdown.classList.remove('show');
+            }
+          }
+        }
+      }
+}
+function tabs(evt, cityName) {
+
+        var i, tabcontent, tablinks;
+      
+        tabcontent = document.getElementsByClassName("tabcontent");
+        for (i = 0; i < tabcontent.length; i++) {
+          tabcontent[i].style.display = "none";
+        }
+      
+        tablinks = document.getElementsByClassName("tablinks");
+        for (i = 0; i < tablinks.length; i++) {
+          tablinks[i].className = tablinks[i].className.replace(" active", "");
+        }
+      
+        document.getElementById(cityName).style.display = "block";
+        evt.currentTarget.className += " active";
+      }
